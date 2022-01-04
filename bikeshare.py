8f19418e2c73af30d8d886a71967712ed3e97157
Markus Brenner <markus.brenner@rwth-aachen.de>
2022-01-04 15:49:17 +0100

    Improved restart query

--- bikeshare.py
+++ bikeshare.py
@@ -318,7 +318,7 @@ def main():
         user_stats(df)
         ind_trip_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Enter "yes" or "no".\n')
         if restart.lower() != 'yes':
             break
 
