fb85b04b4454dce25e854b5ac4f4da9f21162d78
Markus Brenner <markus.brenner@rwth-aachen.de>
2022-01-04 15:23:22 +0100

    Add documentation in main function

--- bikeshare.py
+++ bikeshare.py
@@ -308,6 +308,10 @@ def ind_trip_data(df):
 
 
 def main():
+    """
+    Executes main function
+    """
+    
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
